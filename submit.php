15d596746fc98c100d488e2f6e6fcc72305558d7
Jamie <jamie@blacktraffic.co.uk>
2021-07-09 14:37:01 +0100

    more graphs

--- submit.php
+++ submit.php
@@ -7,7 +7,7 @@ require_once 'inc.php';
 
 <h1>Hashcrack</h1>
 
-<p>Option 1- &quot;I'm feeling lucky&quot;<p> <!--'-->
+<h2>Option 1- &quot;I'm feeling lucky&quot;<p> <!--'--></h2>
 
 <p>Any issues, email or IM jamie.riden@ioactive.co.uk</p>
 
@@ -15,7 +15,7 @@ require_once 'inc.php';
 
   <form method="POST" action="upload.php" enctype="multipart/form-data">
     <div class="upload-wrapper">
-      <span class="file-name">Pick a hash file for cracking...</span><br>
+      <span class="file-name">Pick a hash file for automated cracking...</span><br>
     <p>
       <label for="file-upload"><input type="file" id="file-upload" name="uploadedFile"></label>
     </div>
@@ -24,14 +24,14 @@ require_once 'inc.php';
   </form>
 
 <hr>
-<h3>Option 2: No, I need fine-grained control (work in progress)</h3>
+<h2>Option 2: No, I need fine-grained control</h2>
 
  <form method="POST" action="expert.php" enctype="multipart/form-data">
     <div class="upload-wrapper">
     <p>
       <label for="file-upload"><input type="file" id="file-upload" name="uploadedFile"></label>
                   Optional type override, e.g 1000, or 'ntlm' for NTLM: <label for="mask"><input type="text" name="type"></label>
-<br>At most one mask specified please<br>
+<p><br>At most one mask specified please : <br>
 Optional mask, e.g. ?d?d?d?d?d: <label for="mask"><input type="text" name="mask"></label>
       Optional left mask: <label for="mask"><input type="text" name="lmask"></label>
       Optional right mask: <label for="mask"><input type="text" name="rmask"></label>
@@ -43,7 +43,6 @@ Optional dict override: <select name="dict">
                   require 'dict.php';
                   ?>
 </select>
-<br>
 Optional rule override: <select name="rules">
 
 <?php
@@ -60,7 +59,6 @@ Optional left dict:
 require 'dict.php';
 ?>
 </select>
-<br>
 
 Optional right dict:
 
@@ -86,5 +84,19 @@ print "<pre>Finished: ".`tsp -l | grep finished | wc -l`."</pre>";
 
 print "<h2>GPU status</h2><pre>".`nvidia-smi --query-gpu=gpu_name,temperature.gpu,power.draw,utilization.gpu  --format=csv`."</pre>";
 ?>
+
+<p>
+<img src="./smallbt.png" width=150><br>
+        &nbsp;&nbsp;A black traffic production
+
+<p><p>
+<i><font size=-2>"Numbers is hard and real and they never have feelings<br>
+But you push too hard, even numbers got limits,<br>
+Why did one straw break the camel's back?<br>
+Here's the secret<br>
+The million other straws underneath it<br>
+It's all mathematics"</font></i>
+
 </body>
 </html>
+
